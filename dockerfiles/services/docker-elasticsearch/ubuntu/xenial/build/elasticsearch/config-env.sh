2e20c5041a4e2cff05a65b6a3fab30acbc6da47e
Malcolm Jones <malcolm@adobe.com>
2018-10-12 18:16:00 -0400

    wip: kibana

--- dockerfiles/services/docker-elasticsearch/ubuntu/xenial/build/elasticsearch/config-env.sh
+++ dockerfiles/services/docker-elasticsearch/ubuntu/xenial/build/elasticsearch/config-env.sh
@@ -1,4 +1,4 @@
-export ELASTICSEARCH_YML=6
-export ELASTICSEARCH_JVM_OPTIONS=6
-export FLUENT_CONFIG=6
+export ELASTICSEARCH_YML=7
+export ELASTICSEARCH_JVM_OPTIONS=7
+export FLUENT_CONFIG=7
 export ES_JAVA_OPTS="-XX:InitialHeapSize=256m -XX:MaxHeapSize=512m -XX:+PrintCommandLineFlags -XX:+PrintGC -XX:+PrintGCCause -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintGCDateStamps -XX:+PrintGCApplicationConcurrentTime -XX:+PrintGCApplicationStoppedTime -XX:+PrintTenuringDistribution -XX:+PrintAdaptiveSizePolicy -XX:+AlwaysPreTouch"
